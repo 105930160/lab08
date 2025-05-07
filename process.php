9e93c9589fdb4be5e8d99714058f0234d86111c2
Angel <105930160@student.swin.edu.au>
2025-05-07 14:18:30 +1000

    clean up

--- process.php
+++ process.php
@@ -1,18 +1,12 @@
 <?php
 session_start();
 
-// Get form inputs
 $username = $_POST['username'];
 $password = $_POST['password'];
 
-// Replace these with your actual credentials
-$valid_username = 'Angel';
-$valid_password = '105930160';
-
-if ($username === $valid_username && $password === $valid_password) {
+if ($username === 'Angel'; && $password === '105930160') {
     $_SESSION['user'] = $username;
     header('Location: welcome.php');
-    exit();
 } else {
     header('Location: login.php?error=1');
     exit();
--- welcome.php
+++ welcome.php
@@ -3,13 +3,11 @@ session_start();
 
 if (!isset($_SESSION['user'])) {
     header('Location: login.php');
-    exit();
 }
 
 include 'header.inc';
 ?>
 
 <h1>Welcome, <?php echo htmlspecialchars($_SESSION['user']); ?>!</h1>
-<p>You have successfully logged in.</p>
 
 <?php include 'footer.inc'; ?>
