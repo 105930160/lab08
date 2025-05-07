e507d8771cc9cfc1868e8108a9e84bcda1eeac1a
Angel <105930160@student.swin.edu.au>
2025-05-07 16:53:39 +1000

    Update process.php

--- process.php
+++ process.php
@@ -4,7 +4,7 @@ session_start();
 $username = $_POST['username'];
 $password = $_POST['password'];
 
-if ($username === 'Angel'; && $password === '105930160') {
+if ($username === 'Angel' && $password === '105930160') {
     $_SESSION['user'] = $username;
     header('Location: welcome.php');
 } else {
